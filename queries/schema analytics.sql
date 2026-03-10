9a2c320b4675a3066ed9198f777938432f63c91b
HasmikSargsyan <hasmiksebastia@gmail.com>
2026-03-10 19:24:23 +0400

    update

--- queries/schema analytics.sql
+++ queries/schema analytics.sql
@@ -1,13 +1,13 @@
--- COPY analytics.orders
--- FROM '/data/orders.csv'
--- CSV HEADER;
+COPY analytics.orders
+FROM '/data/orders.csv'
+CSV HEADER;
 
--- SELECT * FROM analytics.orders LIMIT 10;
--- COPY analytics.order_items
--- FROM '/data/order_items.csv'
--- CSV HEADER;
+SELECT * FROM analytics.orders LIMIT 10;
+COPY analytics.order_items
+FROM '/data/order_items.csv'
+CSV HEADER;
 
--- SELECT * FROM analytics.order_items LIMIT 10;
+SELECT * FROM analytics.order_items LIMIT 10;
 SELECT
   COUNT(*) FILTER (WHERE ST_SRID(geom) = 4326) AS correct_srid,
   COUNT(*) AS total_geometries
