c6d211091895480290cba0eaa515e3baabe11cb7
HasmikSargsyan <hasmiksebastia@gmail.com>
2026-03-11 20:31:58 +0400

    update

--- queries/schema analytics.sql
+++ queries/schema analytics.sql
@@ -8,7 +8,3 @@ FROM '/data/order_items.csv'
 CSV HEADER;
 
 SELECT * FROM analytics.order_items LIMIT 10;
-SELECT
-  COUNT(*) FILTER (WHERE ST_SRID(geom) = 4326) AS correct_srid,
-  COUNT(*) AS total_geometries
-FROM analytics.city_boundaries;
