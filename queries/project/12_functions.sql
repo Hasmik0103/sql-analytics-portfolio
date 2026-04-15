6629deec64691dfae2043bc7a54016fc2a0d4c69
HasmikSargsyan <hasmiksebastia@gmail.com>
2026-04-15 20:26:18 +0400

    update

---
+++ queries/project/12_functions.sql
@@ -0,0 +1,54 @@
+SELECT   *
+FROM analytics._stg_world_university_survey;
+
+SELECT
+	MIN(age),
+	MAX(age)
+FROM analytics._stg_world_university_survey;	
+	
+-- GROUP BY AGE GROUP
+
+
+
+SELECT 
+  CASE 
+    WHEN age < 20 THEN 'Under 20'
+    WHEN age BETWEEN 20 AND 24 THEN '20-24'
+    WHEN age BETWEEN 25 AND 29 THEN '25-29'
+    ELSE '30+'
+  END AS age_group,
+  COUNT(*) AS total_students
+FROM analytics._stg_world_university_survey
+GROUP BY age_group
+;
+-- GROUP BY FIELD OF STUDY
+SELECT 
+  field_of_study,
+  COUNT(*) AS total_students
+FROM analytics._stg_world_university_survey
+GROUP BY field_of_study
+ORDER BY total_students DESC;
+
+-- GROUP BY PROGRAM LEVEL
+SELECT 
+  progtam_level,
+  COUNT(*) AS total_students
+FROM analytics._stg_world_university_survey
+GROUP BY progtam_level
+ORDER BY total_students DESC;
+
+-- GROUP BY ONLINE/OFFLINE
+SELECT 
+  online_classes,
+  COUNT(*) AS total_students
+FROM analytics._stg_world_university_survey
+GROUP BY online_classes;
+
+
+-- GROUP BY OVERALL SATISFACTION
+SELECT 
+  overall_satisfaction,
+  COUNT(*) AS total_students
+FROM analytics._stg_world_university_survey
+GROUP BY overall_satisfaction
+ORDER BY total_students DESC;
--- queries/project/6_create_country.sql
+++ queries/project/6_create_country.sql
@@ -1,7 +1,30 @@
 DROP TABLE IF EXISTS analytics.country CASCADE;
 
-CREATE TABLE analytics.country (
-    country_id SERIAL PRIMARY KEY, --autoincrement
-    country_name VARCHAR(50) UNIQUE NOT NULL
-    -- geometry 
+CREATE TABLE analytics._stg_world_countries (
+    country_name TEXT NOT NULL,
+    country_code TEXT NOT NULL,
+    geom geometry(MULTIPOLYGON, 4326) NOT NULL
 );
+
+INSERT INTO analytics._stg_world_countries (country_name, country_code, geom)
+SELECT
+    feature->'properties'->>'name' AS country_name,
+    feature->>'id' AS country_code,
+    ST_SetSRID(
+        ST_Multi(
+            ST_CollectionExtract(
+                ST_Force2D(
+                    ST_MakeValid(
+                        ST_GeomFromGeoJSON(feature->>'geometry')
+                    )
+                ),
+            3)
+        ),
+        4326
+    ) AS geom
+FROM (
+    SELECT jsonb_array_elements(data->'features') AS feature
+    FROM (
+        SELECT pg_read_file('/docker-entrypoint-initdb.d/data/world_university_survey/countries.geo.json')::jsonb AS data
+    ) f
+) sub;
