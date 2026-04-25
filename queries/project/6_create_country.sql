c9a84b8ed1ea8db06d8c3e772f3aa781d6285f48
HasmikSargsyan <hasmiksebastia@gmail.com>
2026-04-25 13:18:49 +0400

    update

--- queries/project/6_create_country.sql
+++ queries/project/6_create_country.sql
@@ -1,18 +1,55 @@
-DROP TABLE IF EXISTS analytics.country CASCADE;
+DROP TABLE IF EXISTS analytics._stg_world_countries CASCADE;
 
 CREATE TABLE analytics._stg_world_countries (
     country_name TEXT NOT NULL,
     country_code TEXT NOT NULL,
     geom geometry(MULTIPOLYGON, 4326) NOT NULL
 );
-INSERT INTO analytics.country (country_name, geom)
-SELECT
-  DISTINCT
-  a. student_country,
-  b. geom
+
+SELECT student_id
+  
 FROM analytics._stg_world_university_survey a
-LEFT JOIN analytics._stg_world_university_survey b on (a.student_country=b.country_name)
-WHERE student_country IS NOT NULL;
+LEFT JOIN analytics._stg_world_countries b on a.country=b.country_name
+WHERE a.country IS NOT NULL;
+
+
+
+SELECT 
+  student_id
+  -- a.country
+  -- b.geom
+FROM analytics._stg_world_university_survey a
+LEFT JOIN analytics._stg_world_countries b 
+  ON a.country = b.country_name
+WHERE a.country IS NOT NULL;
+
+
+
+
+
+
+
+
+SELECT student_id
+FROM analytics._stg_world_university_survey a
+LEFT JOIN analytics._stg_world_countries b 
+  ON a.country = b.country_name
+WHERE a.country IS NOT NULL;
+
+
+
+
+
+
+
+
+
+SELECT * FROM analytics._stg_world_university_survey;
+
+
+
+SELECT * FROM analytics._stg_world_countries;
+
 
 
 INSERT INTO analytics._stg_world_countries (country_name,country_code, geom)
